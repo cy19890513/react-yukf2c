a4df17392287c1d5952680492d1a7e4915a235cb
cy19890513 <yang.chen.sv@gmail.com>
2023-06-08 15:40:02 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-yukf2c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-yukf2c)
---
+++ demo.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+import logo from './logo.png';
+
+export default function BasicRating() {
+  const [value, setValue] = React.useState<number | null>(2);
+
+  return (
+    <Box
+      sx={{
+        '& > legend': { mt: 2 },
+      }}
+    >
+
+      <img src={logo} alt="Logo" />;
+      <Typography component="legend">Controlled</Typography>
+      <Rating
+        name="simple-controlled"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
+      <Typography component="legend">Read only</Typography>
+      <Rating name="read-only" value={value} readOnly />
+      <Typography component="legend">Disabled</Typography>
+      <Rating name="disabled" value={value} disabled />
+      <Typography component="legend">No rating given</Typography>
+      <Rating name="no-value" value={null} />
+
+      <h2>Customer Ratings</h2>
+    </Box>
+  );
+}
